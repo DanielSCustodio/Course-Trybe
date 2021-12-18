cfb112e99a0d5157f6443ebc87d15ddc6f8c9b34
danielm <play.daniel@hotmail.com>
2021-12-18 02:52:08 -0300

    [28] - SQL da aula sobre funções no MySQL

---
+++ Course-Trybe-Backend/03-12-2021[Aula 20.2 - Encontrando dados em um Banco de Dados]/Course-Trybe.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT first_name FROM sakila.actor;
+SELECT distinct release_year from sakila.film;
+select distinct actor_id, first_name, last_name from sakila.actor limit 100 offset 100;
+select count(*) FROM sakila.actor;
+select count(address2) FROM sakila.address;
+	
+
