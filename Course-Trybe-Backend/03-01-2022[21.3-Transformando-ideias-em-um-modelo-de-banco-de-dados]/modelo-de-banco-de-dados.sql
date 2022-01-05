b79a49f9539d0e4b55b482904826d0012874dced
danielm <play.daniel@hotmail.com>
2022-01-05 13:03:24 -0300

    [33]-Modelando banco de dados

---
+++ Course-Trybe-Backend/03-01-2022[21.3-Transformando-ideias-em-um-modelo-de-banco-de-dados]/modelo-de-banco-de-dados.sql
@@ -0,0 +1,78 @@
+create database if not exists albuns;
+create table artista(
+	artista_id int primary key auto_increment,
+    nome varchar(50)
+) engine=InnoDB;
+
+create table album(
+	album_id int primary key auto_increment,
+    artist_id int not null,
+	titulo varchar(100) not null,
+    preco decimal(5,2) not null,
+    estilo_id int not null,
+    foreign key (artist_id) references artista(artista_id),
+    foreign key (estilo_id) references estilomusical(estilo_id)
+)engine=InnoDB;
+
+create table estilomusical(
+	estilo_id int primary key auto_increment,
+    nome varchar(50) not null
+)engine=InnoDB; 
+
+use Biblioteca;
+create database if not exists Biblioteca;
+
+create table Autor (
+	autor_id int primary key auto_increment,
+    nome varchar(45)
+);
+
+create table Categoria (
+	categoria_id int primary key auto_increment,
+    nome varchar(45)
+);
+
+create table Cliente (
+	cliente_id int primary key auto_increment,
+    nome varchar(45)
+);
+
+create table Livro (
+	livro_id int primary key auto_increment,
+    titulo varchar(45),
+    autor_id int not null,
+    categoria_id int not null,
+    foreign key (autor_id) references Autor(autor_id),
+    foreign key (categoria_id) references categoria(categoria_id)
+);
+
+create table Historico(
+	livro_id int not null,
+    cliente_id int not null,
+    constraint primary key (livro_id, cliente_id),
+    foreign key (livro_id)references Livro(livro_id),
+    foreign key (cliente_id) references Cliente (cliente_id)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
