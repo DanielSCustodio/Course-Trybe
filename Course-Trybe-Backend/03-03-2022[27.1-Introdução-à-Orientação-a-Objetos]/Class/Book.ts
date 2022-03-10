46dc7068b38cc13c634eef80eb7fd374eceda317
Daniel Custodio <play.daniel@hotmail.com>
2022-03-10 11:29:39 -0300

    [73] - Introducao a POO - Part 2

--- Course-Trybe-Backend/03-03-2022[27.1-Introdução-à-Orientação-a-Objetos]/Class/Book.ts
+++ Course-Trybe-Backend/03-03-2022[27.1-Introdução-à-Orientação-a-Objetos]/Class/Book.ts
@@ -16,7 +16,7 @@ class Book {
     return this._id;
   }
 
-    get title(): string {
+  get title(): string {
     return this._title;
   }
 
@@ -24,11 +24,21 @@ class Book {
     this._title= title;
   }
 
+    get salePrice(): number {
+    return this._salePrice;
+  }
+
   set salePrice(value:number){
     if(value < this._costPrice){
       console.log('Preço de venda não pode ser menor que o preço de custo')
+    }else{
+      this._salePrice = value;
     }
-    this._salePrice = value;
+  }
+
+  public adjustSalePriceByMarginProfit(marginProfit: number){
+    const newSalePrice = this._costPrice * (1 + marginProfit/100);
+    this._salePrice = newSalePrice;
   }
 
 }
@@ -39,12 +49,16 @@ console.log(book1);
 book1.title = 'Outa história';
 console.log(book1); */
 
-const bookAep = new Book(2,'Aventuras em Paris',15,45);
+/* const bookAep = new Book(2,'Aventuras em Paris',15,45);
 console.log(bookAep);
 
 bookAep.salePrice = 10;
-console.log(bookAep);
+console.log(bookAep); */
 
+const book1 = new Book(1,'Duna', 20, 25);
+console.log(book1.salePrice);
 
+book1.adjustSalePriceByMarginProfit(100);
+console.log(book1.salePrice);
 
 
