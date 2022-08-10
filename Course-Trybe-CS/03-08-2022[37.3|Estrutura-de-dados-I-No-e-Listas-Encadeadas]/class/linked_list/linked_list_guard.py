59c91bcbaed2a8ec447daeed3c4380b875289874
Daniel Custodio <play.daniel@hotmail.com>
2022-08-10 17:01:49 -0300

    [118] - Estrutura de dados - No e listas encadeadas - Part 2

--- Course-Trybe-CS/03-08-2022[37.3|Estrutura-de-dados-I-No-e-Listas-Encadeadas]/class/linked_list/linked_list_guard.py
+++ Course-Trybe-CS/03-08-2022[37.3|Estrutura-de-dados-I-No-e-Listas-Encadeadas]/class/linked_list/linked_list_guard.py
@@ -8,3 +8,26 @@ class LinkedListGuard():
 
     def __str__(self):
         return f"LinkedListGuard(len => {self.__length}, head => {self.head})"
+
+    def insert_first(self, value):
+        first_value = Node(value)
+        first_value.next = self.head
+        self.head = first_value
+
+        if self.__length == 0:
+            self.tail = self.head
+
+        self.__length += 1
+    
+    def insert_last(self, value):
+        if self.is_empty():
+            return self.insert_first(value)
+
+        last_value = Node(value)
+        self.tail.next = last_value
+        self.tail = last_value
+        
+        self.__length += 1
+
+    def is_empty(self):
+        return not self.__length
